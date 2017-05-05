5c2f55a6e64b06e9ba4a13af0d715d01415be0e1
Woontaek <wkblack11@gmail.com>
2017-05-06 01:32:56 +0900

    mod

--- app/src/main/java/com/untaek/oneroom/act/RoomFindMapActivity.java
+++ app/src/main/java/com/untaek/oneroom/act/RoomFindMapActivity.java
@@ -3,6 +3,13 @@ package com.untaek.oneroom.act;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.untaek.oneroom.R;
 
 public class RoomFindMapActivity extends AppCompatActivity {
@@ -11,5 +18,16 @@ public class RoomFindMapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_find_map);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.fragment);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                googleMap.getUiSettings().setRotateGesturesEnabled(false);
+                googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(CameraPosition.fromLatLngZoom(new LatLng(37.209010,126.976408), 14)));
+                googleMap.addMarker(new MarkerOptions().position(new LatLng(37.209010,126.976408)).title("Suwon"));
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/untaek/oneroom/utility/PermissionManager.java
@@ -0,0 +1,9 @@
+package com.untaek.oneroom.utility;
+
+/**
+ * Created by ejdej on 2017-05-06.
+ */
+
+public class PermissionManager {
+
+}
